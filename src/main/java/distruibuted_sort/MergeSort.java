b664566845afb9e9275f989a3eafa69a13f05eb9
rasonjiang <rasonjiang@163.com>
2023-11-28 00:58:58 +0800

    多种排序

--- src/main/java/distruibuted_sort/MergeSort.java
+++ src/main/java/distruibuted_sort/MergeSort.java
@@ -6,9 +6,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class DistributedSort {
+public class MergeSort {
     public static void main(String[] args) {
         // 创建SparkConf对象
         SparkConf conf = new SparkConf().setAppName("DistributedSort").setMaster("local");
@@ -25,11 +26,9 @@ public class DistributedSort {
         JavaRDD<Integer> data;
         data = (JavaRDD<Integer>) sc.parallelize(createData(dataSize));
 
-        // 进行排序
-        JavaRDD<Integer> sortedData ;
-        //Spark API：sortBy排序
-        sortedData = data.sortBy(x -> x, true, dataSize);
-//        sortedData = mergeSort(data);
+        // 使用选择排序自定义比较器
+        JavaRDD<Integer> sortedData;
+        sortedData = mergeSort(data);
 
         // 将结果收集到本地
         System.out.println(sortedData.take(10));  // 打印前10个元素
@@ -50,12 +49,44 @@ public class DistributedSort {
         return data;
     }
 
-    private static JavaRDD<Integer> selectionSort(JavaRDD<Integer> data) {
+    private static JavaRDD<Integer> mergeSort(JavaRDD<Integer> data) {
         return data.mapPartitions(iter -> {
             List<Integer> list = new ArrayList<>();
             iter.forEachRemaining(list::add);
-            Collections.sort(list);
-            return list.iterator();
+            List<Integer> sortedList = mergeSortList(list);
+            return sortedList.iterator();
         }, true);
+    }
+
+    private static List<Integer> mergeSortList(List<Integer> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+
+        int middle = list.size() / 2;
+        List<Integer> left = mergeSortList(list.subList(0, middle));
+        List<Integer> right = mergeSortList(list.subList(middle, list.size()));
+
+        return merge(left, right);
+    }
+
+    private static List<Integer> merge(List<Integer> left, List<Integer> right) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i) <= right.get(j)) {
+                result.add(left.get(i));
+                i++;
+            } else {
+                result.add(right.get(j));
+                j++;
+            }
+        }
+
+        result.addAll(left.subList(i, left.size()));
+        result.addAll(right.subList(j, right.size()));
+
+        return result;
     }
 }
